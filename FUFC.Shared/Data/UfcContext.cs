bfbd97f191512b07777bc903fa799b13fb4edd44
claudeomosa <claudeomosa@gmail.com>
2024-08-05 23:19:47 +0300

    feat: add DB context

---
+++ FUFC.Shared/Data/UfcContext.cs
@@ -0,0 +1,50 @@
+using FUFC.Shared.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace FUFC.Shared.Data;
+
+public class UfcContext : DbContext
+{
+    public UfcContext(DbContextOptions<UfcContext> options) : base(options)
+    {
+    }
+    
+    public DbSet<Gym> Gyms { get; set; }
+
+    public DbSet<Fighter> Fighters { get; set; }
+    
+    public DbSet<Referee> Referees { get; set; }
+    
+    public DbSet<Event> Events { get; set; }
+    
+    public DbSet<Bout> Bouts { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Ignore<FightingStance>();
+        modelBuilder.Ignore<FightingStyle>();
+        modelBuilder.Ignore<WeightClass>();
+        base.OnModelCreating(modelBuilder);
+    }
+}
+
+public class UfcContextFactory : IDesignTimeDbContextFactory<UfcContext>
+{
+    public UfcContext CreateDbContext(string[] args)
+    {
+        // Build the configuration
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .Build();
+
+        // Create options builder
+        var optionsBuilder = new DbContextOptionsBuilder<UfcContext>();
+        optionsBuilder.UseNpgsql(configuration.GetConnectionString("UfcDB"));
+
+        return new UfcContext(optionsBuilder.Options);
+    }
+}
+
