7dbafa8cbd19ba481e5c06de5709f7262748b18a
claudeomosa <claudeomosa@gmail.com>
2024-08-13 01:01:08 +0300

    feat: Official Spider setup -- fetch ranked fighters

--- FUFC.Shared/Models/WeightClass.cs
+++ FUFC.Shared/Models/WeightClass.cs
@@ -2,14 +2,14 @@ namespace FUFC.Shared.Models
 {
     public static class WeightClass
     {
-        public static readonly string Flyweight = "Fly Weight";
-        public static readonly string BantamWeight = "Bantam Weight";
-        public static readonly string FeatherWeight = "Feather Weight";
-        public static readonly string LightWeight = "Light Weight";
-        public static readonly string WelterWeight = "Welter Weight";
-        public static readonly string MiddleWeight = "Middle Weight";
-        public static readonly string LightHeavyWeight = "Light Heavy Weight";
-        public static readonly string HeavyWeight = "Heavy Weight";
+        public static readonly string FlyWeight = "Flyweight";
+        public static readonly string BantamWeight = "Bantamweight";
+        public static readonly string FeatherWeight = "Featherweight";
+        public static readonly string LightWeight = "Lightweight";
+        public static readonly string WelterWeight = "Welterweight";
+        public static readonly string MiddleWeight = "Middleweight";
+        public static readonly string LightHeavyWeight = "Light Heavyweight";
+        public static readonly string HeavyWeight = "Heavyweight";
         public static readonly string Unknown = "";
     }
 }
